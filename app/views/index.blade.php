58e91238f152d76d4a3be975a32c98d0ceed06e8
nicolette <nicolette.williams8@icloud.com>
2016-06-13 11:51:31 -0500

    Everything on index and css thus far

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,15 +10,10 @@
 		<img class="signinimg" src="../img/signin.png">
 	</div>
 
-	<div class="col-md-6">
-		<p class="mission-stmt">
-			Super cool mission statment here. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
+	<div class="col-md-6 mission-col">
+		<p>coding(kids) {</p>
+		<p class="line2mission">var childsFuture = "Priceless";</p>
+		<p>}</p>
 	</div>
 
 </div>
--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,22 +1,24 @@
 <footer class="footer">
-	<div class=" col-md-6 footerinfo FollowUs">
-			<span>Coding Kids <img class="copyright-image" src="/img/copyright.png"></span>
+	<div class="row footerinfo">
+		<div class="col-md-6 followus">
+			<span>Coding Kids© </span>
 			Follow Us On: 
 			<span class="fa fa-twitter"></span>
 			<span class="fa fa-linkedin"></span>
 			<span class="fa fa-facebook"></span>
 			<span class="fa fa-pinterest"></span>
-	</div>
-	<div class="col-md-6 footerinfo">
-		<form class="form-inline">
-				<span class="subscribe">Subscribe To Our Emails</span>
-			 <div class="form-group">
-					<input type="text" class="form-control" id="exampleInputName2" placeholder="name">
-			 </div>
-			 <div class="form-group">
-					<input type="email" class="form-control" id="exampleInputEmail2" placeholder="email">
-			 </div>
-			 <button type="submit" class="btn btn-info">SUBMIT</button>
-		</form>
+		</div>
+		<div class="col-md-6">
+			<form class="form-inline">
+					<span class="subscribe">Subscribe To Our Emails</span>
+				 <div class="form-group">
+						<input type="text" class="form-control" id="exampleInputName2" placeholder="name">
+				 </div>
+				 <div class="form-group">
+						<input type="email" class="form-control" id="exampleInputEmail2" placeholder="email">
+				 </div>
+				 <button type="submit" class="btn btn-info">SUBMIT</button>
+			</form>
+		</div>
 	</div>
 </footer>	
