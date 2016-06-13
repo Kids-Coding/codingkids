8df8a052eb0e23a19548fd2eeada1452213f1007
nicolette <nicolette.williams8@icloud.com>
2016-06-13 10:35:57 -0500

    some of the index page, and css

---
+++ app/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="video"></div>
+
+<div class="row">
+	<div class="col-md-6">
+		<img class="signupimg" src="../img/signup.png">
+		<img class="signinimg" src="../img/signin.png">
+	</div>
+
+	<div class="col-md-6">
+		<p class="mission-stmt">
+			Super cool mission statment here. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+
+</div>
+
+
+
+@stop
