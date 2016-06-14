f9566f8c6aa383681fd0c599cc9c0b4eff6cceca
nicolette <nicolette.williams8@icloud.com>
2016-06-14 12:30:41 -0500

    only able to view lessons page when logged in

--- app/controllers/LessonsController.php
+++ app/controllers/LessonsController.php
@@ -4,8 +4,13 @@ class LessonsController extends BaseController {
 
     public function index()
         {
-            $lesson = Lesson::all();
-            return View::make('lesson.index')->withLesson($lesson);
+            if(Auth::check()){
+                $lesson = Lesson::all();
+                return View::make('lesson.index')->withLesson($lesson);
+            } else {
+                Session::flash('errorMessage', 'You must be logged in to view this page.');
+                return Redirect::action('HomeController@showIndex');
+            }
         }
 
     public function show($id)
