f5dd6ce526c20f158c12170f7df825dd6af235fc
nicolette <nicolette.williams8@icloud.com>
2016-06-14 14:44:50 -0500

    fixed marcus' terrible indentation

--- app/views/Lesson/index.blade.php
+++ app/views/Lesson/index.blade.php
@@ -7,46 +7,37 @@ this is the lessons page
 
 <div>
     <br>
-    
+
     <div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action <span class="caret"></span>
-    </button>
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">HTML <span class="caret"></span></button>
         <ul class="dropdown-menu">
-    @foreach ($lessons as $lesson)
-    @if ($lesson->category == 'html')
-    html
-        <li><a href="{{action('LessonsController@show', $lesson->id)}}">{{$lesson->name}}</a></li>
-    @endif
-    @endforeach
+        @foreach ($lessons as $lesson)
+            @if ($lesson->category == 'html')
+                <li><a href="{{action('LessonsController@show', $lesson->id)}}">{{$lesson->name}}</a></li>
+            @endif
+        @endforeach
         </ul>
     </div>
 
-   <div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action <span class="caret"></span>
-    </button>
+    <div class="btn-group">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CSS <span class="caret"></span></button>
         <ul class="dropdown-menu">
-    @foreach ($lessons as $lesson)
-    @if ($lesson->category == 'css')
-    css
-        <li><a href="{{action('LessonsController@show', $lesson->id)}}">{{$lesson->name}}</a></li>
-    @endif
-    @endforeach
+        @foreach ($lessons as $lesson)
+            @if ($lesson->category == 'css')
+                <li><a href="{{action('LessonsController@show', $lesson->id)}}">{{$lesson->name}}</a></li>
+            @endif
+        @endforeach
         </ul>
     </div>
 
-<div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action <span class="caret"></span>
-    </button>
+    <div class="btn-group">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PHP<span class="caret"></span></button>
         <ul class="dropdown-menu">
-    @foreach ($lessons as $lesson)
-    @if ($lesson->category == 'php')
-    php
-        <li><a href="{{action('LessonsController@show', $lesson->id)}}">{{$lesson->name}}</a></li>
-    @endif
-    @endforeach
+        @foreach ($lessons as $lesson)
+            @if ($lesson->category == 'php')
+                <li><a href="{{action('LessonsController@show', $lesson->id)}}">{{$lesson->name}}</a></li>
+            @endif
+        @endforeach
         </ul>
     </div>
 </div>
