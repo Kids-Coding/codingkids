3402280df9f3bb79b6b1fdb7a53d366136d54164
nicolette <nicolette.williams8@icloud.com>
2016-06-15 13:38:02 -0500

    got rid of those 90's style buttons just for isaac

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,9 +5,9 @@
 <img class="video" src="../img/videoholder.jpg">
 
 <div class="row">
-	<div class="col-md-6">
-		<a href="/signup"><img class="signupimg" src="../img/signup.png"></a>
-		<a href="/login"><img class="signinimg" src="../img/signin.png"></a>
+	<div class="col-md-6 about">
+		<p class="whyck">Why Coding Kids?</p>
+		<p>In today’s digital world, coding is a fundamental skill alongside math and reading, but too few kids have the opportunity to learn to program because it is rarely taught in school. Coding Kid’s mission is to create a fun and engaging platform where children not only learn to program, but also have opportunities to be creative using code.</p>
 	</div>
 
 	<div class="col-md-6 mission-col">
