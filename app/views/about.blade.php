10b4797f9f7eec7eb531d84f96b2737c7d5a5c61
Jerod West <jerod.west@gmail.com>
2016-06-24 08:15:50 -0500

    worked on my about me

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -23,7 +23,7 @@
         <div class="col-md-4">
             <img src="../img/jerod.png" height="300">
             <div class="names">Jerod West</div>
-            <div class="aboutfounders">dl;sahg;kdjsh;asdkga sdlkhf;asldk asd;kjahsd;kas bd;gkjasdh;fkajshdf ;aksjdhg;k ajsdf;kajshdf;kasjdhf;k a;kjsdhg;akjsdhf;kajsdfkajds s;dkjfhg as;dkjghakdsghaskdjg a;skdjfh;sk djgh;aksdjhg;askdj ;aksdjhg;askdjhg;aksjdhg;askjd ;askdjgha;skdhg ;kajsdhg;ajk ;kfdjhg;akjsdhf;aksjdf asdjfh;a sdkhg;asdlkjgasdlk a;ksdjhg;kasdh;</div>
+            <div class="aboutfounders"> I live and exist for my wife and two children . I love learning, and teaching others. These days it seems our children are growing faster than technology and fashion trends.  I would love for every child to have the opportunity to be introduced at an early age to coding. As aformentioned above, I have a wife and two children I love with everything I am. Some of my favorite hobbies include: cooking, playing guitar, helping others, and of course having fun ( I am quite the accomplished prankster ). I hope this site serves as a usefull tool in developing a basic understanding into the world of coding.</div>
         </div> 
     </div>
     <div class="center">
