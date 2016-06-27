ee37f99641136fb04f3662d74f56c6c72f442b86
Marcus <mmond21@gmail.com>
2016-06-27 15:01:35 -0500

    added some stuff to the summary

--- app/views/quizzes/summary.blade.php
+++ app/views/quizzes/summary.blade.php
@@ -5,11 +5,14 @@
     <div class="row">
 
         <!-- title -->
-        <div style='margin-left: -875px;'>
-            <p>
+        <div style='margin-left: -600px;'>
+            <h4>
               &lt;!doctype&gt; - Needs to be at the very top.<br>
-              &lt;html&gt; &lt;/html&gt; - An example of opening and closing tags
-            </p>
+              Defines the document type. <br><br>
+              &lt;html&gt; &lt;/html&gt; - An example of opening and closing tags. <br>
+              Remember there are many more tags including <br> 
+              header tags, body tags, and paragraph tags.
+            </h4>
         </div> <!-- end col/title -->
 
     </div> <!-- end row -->
