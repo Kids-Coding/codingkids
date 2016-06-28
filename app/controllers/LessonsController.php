b29409095cd3896353a450d2dd1564bbf111a2c0
nicolette <nicolette.williams8@icloud.com>
2016-06-27 21:56:36 -0500

    working progress bar, yay

--- app/controllers/LessonsController.php
+++ app/controllers/LessonsController.php
@@ -1,15 +1,16 @@
 <?php 
 class LessonsController extends BaseController {
     public function index()
-        {
-            if(Auth::check()){
-                $lessons = Lesson::all();
-                return View::make('lesson.index')->with('lessons', $lessons);
-            } else {
-                Session::flash('errorMessage', 'You must be logged in to view this page.');
-                return Redirect::action('HomeController@showIndex');
-            }
+    {
+        if(Auth::check()){
+            $lessons = Lesson::all();
+            return View::make('lesson.index')->with('lessons', $lessons);
+        } else {
+            Session::flash('errorMessage', 'You must be logged in to view this page.');
+            return Redirect::action('HomeController@showIndex');
         }
+    }
+
     public function show($id)
     {
         $lesson = Lesson::find($id);
@@ -17,9 +18,17 @@ class LessonsController extends BaseController {
         return View::make('lesson.show')->with('lesson', $lesson)->with('allLessons', $allLessons);
     }
 
-    public function checkQuiz()
+    public function completeLesson($id)
     {
-        return 'hey you';
+
+        $lesson = Lesson::find($id);
+        $allLessons = DB::table('lessons')->where('category', $lesson->category)->get();
+        $finishedLesson = DB::table('lesson_user')->insert(
+            array('lesson_id' => $id, 'user_id' => Auth::id())
+        );
+
+        Session::flash('successMessage', 'Lesson Completed Successfully');
+        return View::make('lesson.show')->with('lesson', $lesson)->with('allLessons', $allLessons);
     }
 
 }
